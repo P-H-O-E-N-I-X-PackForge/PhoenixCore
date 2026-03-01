efdbab5ec84ba714022a5c8c20eb6963e8c14a82
Connor Smith <phoenixvine32908@gmail.com>
2026-03-01 12:36:58 -0500

    jsons

---
+++ src/main/java/net/phoenix/core/common/data/item/TeslaStabilizerItem.java
@@ -0,0 +1,21 @@
+package net.phoenix.core.common.data.item;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class TeslaStabilizerItem extends Item {
+    public TeslaStabilizerItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
+        tooltip.add(Component.translatable(this.getDescriptionId() + ".tooltip"));
+    }
+}
